0e7754c741f16de1315451f8e5c0d8bd932a015a
Pam Deininger <pameladeininger@gmail.com>
2019-12-01 15:37:59 -0500

    corrected code

---
+++ codeQuizOne/cQ1.js
@@ -0,0 +1,91 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+ 
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+ 
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+ 
+    this.questionIndex++;
+}
+ 
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+ 
+ 
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+ 
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+ 
+ 
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        // show question
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+ 
+        // show options
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+ 
+        showProgress();
+    }
+};
+ 
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+ 
+ 
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+ 
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+ 
+// create questions here
+var questions = [new Question("Who was originally cast as the Tin Man in the Wizard of Oz?", ["Don Ameche", "Buddy Ebsen","Mel Blanc", "Fred MacMurray"], "Buddy Ebsen"),
+    new Question("Judy Garland, the actress who played Dorothy, is the mother of which of the following famous people?", ["Bette Midler", "Hayley Mills", "Liza Minnelli", "Susan Sarandon"], "Liza Minnelli"),
+    new Question("How many characters did actor Frank Morgan play?", ["5", "1","None", "3"], "3"),
+    new Question("What is the name of Dorothy's dog?", ["Benji", "Lassie", "Bud", "Toto"], "Toto"),
+    new Question("What role did Billie Burke play?", ["Glinda the Good Witch", "The Wizard", "Uncle Henry", "Miss Gulch"], "Glinda the Good Witch"),
+    new Question("What is the name of Dorothy's aunt", ["Auntie Em", "Aunt Sue", "Auntie", "Aunt Caroline"], "Auntie Em")
+];
+
+// create quiz
+var quiz = new Quiz(questions);
+ 
+// display quiz
+populate();
