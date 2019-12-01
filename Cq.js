47caaa7609f737c4c30e7e76391affd83618ebcf
Pam Deininger <pameladeininger@gmail.com>
2019-11-30 19:32:15 -0500

    updates to content

---
+++ Cq.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+
+var quizPlay = prompt("Ready for a quiz?");
+var confirmPlay = confirm("Let's get started");
+
+var one = ["cascadingStyleSheets", "cascadingSyntaxSheets", "cascadingStyleSyntax", "calculatedStyleSheet"];
